e0c34c4a31193d94a1cbc336fab72f733116c353
Azhdar Aliev <stud.stud.info@gmail.com>
2023-02-10 23:48:43 +0300

    initial

---
+++ Test02.py
@@ -0,0 +1,20 @@
+students = int(input("Сколько учеников в клвссе? Введите число: "))
+counter_4 = 0
+counter_5 = 0
+counter_3 = 0
+for classman in range(1, students+1):
+    scores = int(input(f"Какю оценку получил ученик {classman}? "))
+    if scores == 4:
+        counter_4 += 1
+    elif scores == 3:
+        counter_3 += 1
+    elif scores == 5:
+        counter_5 += 1
+if counter_5 < counter_4 > counter_3:
+    print("Хорошистов сегодня больше")
+elif counter_3 < counter_5 > counter_4:
+    print("Отличников сегодня больше")
+else:
+    print("Троечников сегодня больше")
+
+print()
