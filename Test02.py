724998cc1fea2104fcc0d59ecd0b277dcbeffdc1
Azhdar Aliev <stud.stud.info@gmail.com>
2023-02-11 23:59:32 +0300

    night

--- Test02.py
+++ Test02.py
@@ -3,11 +3,8 @@ counter_4 = 0
 counter_5 = 0
 counter_3 = 0
 for classman in range(1, students+1):
-<<<<<<< HEAD
     scores = int(input(f"Какю ОТМЕТКУ получил студент {classman}? "))
-=======
     scores = int(input(f"Какю оценку получил студент сегодня {classman}? "))
->>>>>>> a92120bb63978821f8df90ba4251cbe7b0a26dd6
     if scores == 4:
         counter_4 += 1
     elif scores == 3:
