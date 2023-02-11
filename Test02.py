20c089085f75914f1e4eb88d4aba667dbfa0a21a
Azhdar Aliev <stud.stud.info@gmail.com>
2023-02-11 19:25:34 +0300

    added last print + changed int

--- Test02.py
+++ Test02.py
@@ -3,7 +3,7 @@ counter_4 = 0
 counter_5 = 0
 counter_3 = 0
 for classman in range(1, students+1):
-    scores = int(input(f"Какю оценку получил студент {classman}? "))
+    scores = int(input(f"Какю ОТМЕТКУ получил студент {classman}? "))
     if scores == 4:
         counter_4 += 1
     elif scores == 3:
@@ -15,6 +15,7 @@ if counter_5 < counter_4 > counter_3:
 elif counter_3 < counter_5 > counter_4:
     print("Отличников сегодня больше")
 else:
-    print("Троечников сегодня больше ВСЕХ")
+    print("троечников сегодня больше ВСЕХ")
+    print('Добавленная строка')
 
 print()
